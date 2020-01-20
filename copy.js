055287c75ba23c5060d1344ccfe05e5b9d0b20f7
jiangwenbin <wenbin.jiang@coollu.com.cn>
2020-01-20 14:34:40 +0800

    海报修改

--- copy.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-const fileReadStream = fs.createReadStream('README.md');
-const fileWriteStream = fs.createWriteStream('_book/README.md');
-fileReadStream.pipe(fileWriteStream);
-fileWriteStream.on('close', function(){
-    console.log('复制完成');  
-})
